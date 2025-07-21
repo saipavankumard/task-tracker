c32560a78f26e73b432238d03d841217523d35be
Pavan Kumar <sai.pavankumar@tavant.com>
2025-07-21 11:06:13 +0530

    Add dummy Test Case

---
+++ src/test/java/com/example/quaterlythree/DummyTest.java
@@ -0,0 +1,12 @@
+package com.example.quaterlythree;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DummyTest {
+    @Test
+    void testDummy(){
+        assertTrue(1+1 == 2);
+    }
+}
